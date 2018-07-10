937c7f3b3c19955d9a77c26558ef204e8d895a58
Kuba <malinogit@gmail.com>
2018-07-11 00:17:03 +0200

    initial commit

---
+++ src/main/java/pl/mal/gitflowtest/GitFlowTestApplication.java
@@ -0,0 +1,12 @@
+package pl.mal.gitflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowTestApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/mal/gitflowtest/GitFlowTestApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mal.gitflowtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitFlowTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
