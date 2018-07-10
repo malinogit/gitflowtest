8f331cbfa8f0b5b498c1363a41ef7bddcffdedce
Kuba <malinogit@gmail.com>
2018-07-11 01:31:37 +0200

    #123123 - fafw

--- src/main/java/pl/mal/gitflowtest/init/TestInti.java
+++ src/main/java/pl/mal/gitflowtest/init/TestInti.java
@@ -8,9 +8,11 @@ public class TestInti implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        String fa = "fafa";
+        String fw = "fwfw";
         String s = "dziala";
         System.out.println(s + s);
-        System.out.println(s + s);
+        System.out.println(fa + fw);
         System.out.println(s + s);
     }
 }
